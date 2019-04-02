d031f7de4e18659c5420504462091c46ca71f29a
Jyotiraditya <jyotiraditya182@gmail.com>
2019-04-02 23:21:18 +0530

    mido: fredOS bringup.
    Signed-off-by: Jyotiraditya <jyotiraditya182@gmail.com>

--- fred_mido.mk
+++ fred_mido.mk
@@ -21,8 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common FredOS stuff.
+$(call inherit-product, vendor/fred/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
